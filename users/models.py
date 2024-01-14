b52b8eade1016c6005c682fad58c2d87bae23db2
Daria <DashaGurieva@mail.ru>
2024-01-14 11:23:10 +0500

    add app users, .gitignore, requirements.txt

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
